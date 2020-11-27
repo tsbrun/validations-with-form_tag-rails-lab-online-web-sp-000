17237d457a8dc1e1576876bf852389285736080b
TSephanya Brun <tsbrun@icloud.com>
2020-11-27 14:31:42 -0500

    commit tree d48218c2927bcd2be6f05772df47dc3917bf806c

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-    validates :title, presence: { message: "Post must have a title." }
-    validates :content, length: { minimum: 100, message: "Content must be at least %{count} characters." }
-    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "Category must be 'Fiction' or 'Non-Fiction'." }
+    validates :title, presence: { message: "cannot be blank." }
+    validates :content, length: { minimum: 100, message: "must be at least %{count} characters." }
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction), message: "must be 'Fiction' or 'Non-Fiction'." }
 end
