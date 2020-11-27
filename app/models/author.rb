2d59b4fb747dd647565388ce0a28a83384042b99
TSephanya Brun <tsbrun@icloud.com>
2020-11-27 14:31:45 -0500

    Done.

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,5 +1,5 @@
 class Author < ActiveRecord::Base
-    validates :name, presence: { message: "Name cannot be blank." }
-    validates :email, uniqueness: { message: "Email must be unique." }
-    validates :phone_number, length: { is: 10, message: "Phone number must be %{count} digits long." }
+    validates :name, presence: { message: "cannot be blank." }
+    validates :email, uniqueness: { message: "must be unique." }
+    validates :phone_number, length: { is: 10, message: "must be %{count} digits long." }
 end
